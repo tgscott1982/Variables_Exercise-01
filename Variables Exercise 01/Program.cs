2fb053e3a59eb27a4ded5d84074823f1fb36293f
Travis Gersch <t_gersch@hotmail.com>
2022-07-14 20:16:25 -0500

    variable extra 1 finished

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Variables_Exercise_01
 {
@@ -11,6 +12,11 @@ namespace Variables_Exercise_01
 
             // TODO: Declare 3 constansts
 
+            const int FinalFantasy = 7;
+            const decimal Bday = 7.16m;
+            const string HP = "IsolemnlyswearthatImuptonogood";
+
+
             #region Types
             // string
             // char
@@ -22,10 +28,22 @@ namespace Variables_Exercise_01
 
             // TODO: Declare a variable for each of the types above
             // Make sure to use C# naming convention best practices 
-
+            string a;
+            char b;
+            bool c;
+            int d;
+            double e;
+            decimal f;
             // TODO: Initialize each of the variables with a value
-
+            a = "FinalFantasy7";
+            b = 's';
+            c = false;
+            d = 82;
+            e = 10.2;
+            f = 1.234567890m;
             // TODO: Write out each of the variables to the Console 
+            Console.WriteLine($"{a}, {b}, {c}, {d}, {e}, {f}");
+
         }
 
     }
